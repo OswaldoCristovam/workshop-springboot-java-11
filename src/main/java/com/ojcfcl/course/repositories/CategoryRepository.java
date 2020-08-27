3e3f67cbc95315795bfa7af246c99fc3671f50f9
Oswaldo Cristovam <oswaldo.jose.c@hotmail.com>
2020-08-27 17:16:24 -0300

    Category

---
+++ src/main/java/com/ojcfcl/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.ojcfcl.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ojcfcl.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
